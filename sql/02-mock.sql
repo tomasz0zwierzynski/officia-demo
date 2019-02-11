07f948fc9b0d7fb4b2ecf11cd7aca20239f90b9a
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2019-02-11 16:07:19 +0100

    Added new subjects

---
+++ sql/02-mock.sql
@@ -0,0 +1,7 @@
+INSERT INTO subject (id, name) VALUES (1, 'Matematyka');
+
+INSERT INTO subject (id, name) VALUES (2, 'Fizyka');
+
+INSERT INTO subject (id, name) VALUES (3, 'Chemia');
+
+INSERT INTO subject (id, name) VALUES (4, 'Informatyka');
