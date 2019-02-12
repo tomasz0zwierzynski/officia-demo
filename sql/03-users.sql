ccbd65cf624899421364bec77cb8c064c0be964d
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2019-02-12 15:43:39 +0100

    Added new users with roles to database

---
+++ sql/03-users.sql
@@ -0,0 +1,183 @@
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  5,  'tomasz',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Tomasz',  'Zwierzynski',  'tomasz@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  5,  'ROLE_TUTOR'
+);
+
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  6,  'marcin',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Marcin',  'Musielak',  'marcin@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+  INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  6,  'ROLE_TUTOR'
+);
+
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  7,  'lukasz',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Lukasz',  'Bojarski',  'lukasz@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+  INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  7,  'ROLE_TUTOR'
+);
+
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  8,  'kasia',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Kasia',  'Kowal',  'kasia@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+  INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  8,  'ROLE_STUDENT'
+);
+
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  9,  'basia',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Basia',  'Kowal',  'basia@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+  INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  9,  'ROLE_STUDENT'
+);
+
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  10,  'marysia',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Marysia',  'Kowal',  'marysia@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+  INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  10,  'ROLE_STUDENT'
+);
+
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  11,  'krysia',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Krysia',  'Kowal',  'krysia@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+  INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  11,  'ROLE_STUDENT'
+);
+
+INSERT INTO
+  public.jhi_user
+(
+  id,  login,  password_hash,  first_name,  last_name,  email,  image_url,
+  activated,  lang_key,  activation_key,  reset_key,  created_by,  created_date,
+  reset_date,  last_modified_by,  last_modified_date
+)
+VALUES (
+  12,  'zdzisia',
+  '$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K',
+  'Zdzisia',  'Kowal',  'zdzisia@localhost',
+  null,  true,  'en',  null,  null,  'system',  null,  null,  'system',  null
+  );
+
+  INSERT INTO
+  public.jhi_user_authority
+(
+  user_id,  authority_name
+)
+VALUES (
+  12,  'ROLE_STUDENT'
+);
