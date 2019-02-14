887b19789939b40236eb9df019522b25f48bf467
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2019-02-14 11:09:04 +0100

    Dodano Tutorów i Studentów

---
+++ sql/04-tutors.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+  public.tutor (  id,  fullname,  user_id )
+VALUES (  1,  'Tomasz Zwierzyński',  5 );
+
+INSERT INTO
+  public.tutor (  id,  fullname,  user_id )
+VALUES (  2,  'Marcin Musielak',  6 );
+
+INSERT INTO
+  public.tutor (  id,  fullname,  user_id )
+VALUES (  3,  'Łukasz Bojarski',  7 );
---
+++ sql/05-students.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+  public.student ( id, fullname, user_id )
+VALUES ( 1, 'Kasia Kowal', 8 );
+
+INSERT INTO
+  public.student ( id, fullname, user_id )
+VALUES ( 2, 'Basia Kowal', 9 );
+
+INSERT INTO
+  public.student ( id, fullname, user_id )
+VALUES ( 3, 'Marysia Kowal', 10 );
+
+INSERT INTO
+  public.student ( id, fullname, user_id )
+VALUES ( 4, 'Krysia Kowal', 11 );
+
+INSERT INTO
+  public.student ( id, fullname, user_id )
+VALUES ( 5, 'Zdzisia Kowal', 12 );
