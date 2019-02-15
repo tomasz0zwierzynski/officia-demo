748c8ddc029ce85d7f274b72b09ed993551f2ce9
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2019-02-15 14:23:03 +0100

    Dodano komponent kalendarza

--- src/main/webapp/app/available/available.module.ts
+++ src/main/webapp/app/available/available.module.ts
@@ -1,14 +1,26 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IterateSharedModule } from '../shared';
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { AVAILABLE_ROUTE, AvailableComponent } from './';
 import { PageOneComponent } from './page-one/page-one.component';
 import { PageTwoComponent } from './page-two/page-two.component';
 
 @NgModule({
-    imports: [IterateSharedModule, RouterModule.forRoot([AVAILABLE_ROUTE], { useHash: true })],
+    imports: [
+        IterateSharedModule,
+        RouterModule.forRoot([AVAILABLE_ROUTE], { useHash: true }),
+        BrowserAnimationsModule,
+        CalendarModule.forRoot({
+            provide: DateAdapter,
+            useFactory: adapterFactory
+        })
+    ],
     declarations: [AvailableComponent, PageOneComponent, PageTwoComponent],
     entryComponents: [],
     providers: [],
--- src/main/webapp/app/available/page-one/page-one.component.ts
+++ src/main/webapp/app/available/page-one/page-one.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
+import { Subject } from 'rxjs';
+import { startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours } from 'date-fns';
+
+const colors: any = {
+    red: {
+        primary: '#ad2121',
+        secondary: '#FAE3E3'
+    },
+    blue: {
+        primary: '#1e90ff',
+        secondary: '#D1E8FF'
+    },
+    yellow: {
+        primary: '#e3bc08',
+        secondary: '#FDF1BA'
+    }
+};
 
 @Component({
     selector: 'jhi-page-one',
@@ -7,6 +25,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageOneComponent implements OnInit {
     message: string;
+
+    viewDate: Date = new Date();
+
+    modalData: {
+        action: string;
+        event: CalendarEvent;
+    };
+
+    actions: CalendarEventAction[] = [
+        {
+            label: '<i class="fa fa-fw fa-pencil"></i>',
+            onClick: ({ event }: { event: CalendarEvent }): void => {
+                // this.handleEvent('Edited', event);
+            }
+        },
+        {
+            label: '<i class="fa fa-fw fa-times"></i>',
+            onClick: ({ event }: { event: CalendarEvent }): void => {
+                this.events = this.events.filter(iEvent => iEvent !== event);
+                // this.handleEvent('Deleted', event);
+            }
+        }
+    ];
+
+    refresh: Subject<any> = new Subject();
+
+    events: CalendarEvent[] = [
+        {
+            start: subDays(startOfDay(new Date()), 1),
+            end: addDays(new Date(), 1),
+            title: 'A 3 day event',
+            color: colors.red,
+            actions: this.actions,
+            allDay: true,
+            resizable: {
+                beforeStart: true,
+                afterEnd: true
+            },
+            draggable: true
+        },
+        {
+            start: startOfDay(new Date()),
+            title: 'An event with no end date',
+            color: colors.yellow,
+            actions: this.actions
+        },
+        {
+            start: subDays(endOfMonth(new Date()), 3),
+            end: addDays(endOfMonth(new Date()), 3),
+            title: 'A long event that spans 2 months',
+            color: colors.blue,
+            allDay: true
+        },
+        {
+            start: addHours(startOfDay(new Date()), 2),
+            end: new Date(),
+            title: 'A draggable and resizable event',
+            color: colors.yellow,
+            actions: this.actions,
+            resizable: {
+                beforeStart: true,
+                afterEnd: true
+            },
+            draggable: true
+        }
+    ];
+
+    activeDayIsOpen = true;
 
     constructor() {
         this.message = 'PageOneComponent message';
